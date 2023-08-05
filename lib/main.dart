367e59d44d53b89f7c7a953d1f7c35dc4b1d6a61
puchochiri <doawishfor@naver.com>
2023-08-05 16:56:58 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  // Flutter 앱이 실향될 준비가 됬는지 확인
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //
+}
